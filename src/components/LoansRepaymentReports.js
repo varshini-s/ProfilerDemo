bad42cc47412313833249fbb92335a19199a16c0
varshini <dreamvarshini@gmail.com>
2021-11-23 01:08:50 +0530

    ADDED: loans repayment component

---
+++ src/components/LoansRepaymentReports.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class LoansRepaymentReports extends Component {
+    render() {
+        return (
+            <div>
+                Welcome to loans repayment reports
+            </div>
+        )
+    }
+}
+
+export default LoansRepaymentReports
